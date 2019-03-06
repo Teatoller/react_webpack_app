438df25268d2f4b7d7e21d1fec42a947c4997260
Teatoller <steven.otieno1@gmail.com>
2019-03-06 10:20:18 +0300

    (screensaver): implement screensaver component
    - add screensaver ui
    
    (#009)

---
+++ src/components/ScreenSaver.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class ScreenSaver extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+		<div class="header">
+			<h1 class="img-align">Store Manager</h1>
+		</div>
+	</header>
+	<div class="containerlog">
+		<form>
+			<a href="login.html" class="button">Enter</a>
+			
+		</form>
+		
+	</div>
+<footer class="containerfooter"><small>AC-33 Copyright &copy;2018</small></footer>
+      </div>
+    )
+  }
+}
+
+export default ScreenSaver;
