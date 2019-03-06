41461515ce3c514ef822615327ce8a38a81a504a
Teatoller <steven.otieno1@gmail.com>
2019-03-06 10:39:43 +0300

    (route): implement login route
    - add route for login in app.js file
    
    (#012)

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import HomeComponent from './HomeComponent';
 import ScreenSaver from './ScreenSaver';
+import login from './login'
 import '../public/css/style.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
                     <Switch>
                     <Route exact={true} path='/' component={HomeComponent} />
                     <Route exact path='/screenSaver' component={ScreenSaver} />
+                    <Route exact path='/login' component={login} />
                     </Switch>
                 </div>
             </Router>
