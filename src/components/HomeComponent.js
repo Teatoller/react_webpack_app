e8e1dc1f181772619aa4e656c45e93aee48f9375
Teatoller <steven.otieno1@gmail.com>
2019-03-06 10:03:40 +0300

    (homecomponent) implement home component
    - rename the home component
    
    (#009)

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 
-export default class HomeComponent extends Component {
+class HomeComponent extends Component {
     render() {
         return (
             <div>
                 <header>
-                    <div class="headers">
-                        <h1 class="img-align">Store Manager</h1>
+                    <div className="headers">
+                        <h1 className="img-align">Store Manager</h1>
                     </div>
                 </header>
-                <div class="main-menu">
-                    <a href="#" class="active">Home</a>
+                <div className="main-menu">
+                    <a href="#" className="active">Home</a>
                     <a href="vwAdmin.html">Admin</a>
                     <a href="sattendant.html">Store Attendant</a>
                     <a href="vwSocketGenInventory.html">Reports</a>
@@ -22,10 +22,11 @@ export default class HomeComponent extends Component {
                     <a href="#"> </a>
                     <a href="#"> </a>
                 </div>
-                <footer class="temp-footer"><small>AC-33 &copy;2018</small>
+                <footer className="temp-footer"><small>AC-33 &copy;2018</small>
                 </footer>
             </div>
         )
     }
 }
 
+export default HomeComponent;
